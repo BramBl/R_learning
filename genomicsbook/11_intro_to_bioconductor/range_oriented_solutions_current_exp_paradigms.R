602aa24624c821236f68526ae6ce8ae8c9468435
BramBl <brambloemen1@gmail.com>
2021-04-26 22:57:18 +0200

    intro into summarized experiments (RNAseq etc --> range-based)

---
+++ genomicsbook/11_intro_to_bioconductor/range_oriented_solutions_current_exp_paradigms.R
@@ -0,0 +1,62 @@
+.libPaths('C:/Users/BrBl1834/R/win-library')
+
+
+# General considerations --------------------------------------------------
+
+library(limma)
+library(Biobase)
+library(data.table)
+library(GEOquery)
+library(erma)
+library(RNAseqData.HNRNPC.bam.chr14)
+library(airway)
+library(annotate)
+library(minfi)
+library(locfit)
+library(IlluminaHumanMethylation450kmanifest)
+library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+library(BiocStyle)
+library(GenomicFiles)
+library(GenomicAlignments)
+library(MultiAssayExperiment)
+library(RaggedExperiment)
+library(VariantAnnotation)
+library(VariantTools)
+library(bigrquery)
+library(dplyr)
+library(magrittr)
+library(curatedTCGAData)
+library(ArrayExpress)
+
+methods(class="SummarizedExperiment")
+browseVignettes("SummarizedExperiment")
+
+library(airway)
+data(airway)
+airway
+
+metadata(airway)
+
+colData(airway)
+rowRanges(airway)
+
+rowRanges(airway)$ENSG00000172057
+
+# see to how many unique exons/ranges the diff rows can be reduced
+reduce(rowRanges(airway)$ENSG00000172057)
+
+
+names(colData(airway))
+
+table(airway$dex) # main treatment factor
+
+
+
+# Handling the ArrayExpress deposit of Illumina 450k Methylation arrays --------
+
+
+# run previous script from course to load the ArrayExpress for next part of exercise
+source("./management_genome_scale_exp.R")
+library(data.table)
+sd5797 = fread("E-MTAB-5797.sdrf.txt")
+head(sd5797[,c(3,16,18)])
